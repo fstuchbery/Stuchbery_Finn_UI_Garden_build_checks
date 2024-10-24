793b1bf34a1fed3438f227c96bb4b5e556731c11
fstuchbery <163586465+fstuchbery@users.noreply.github.com>
2024-10-24 12:20:00 -0500

    this is a test to see if husky will work

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -14,7 +14,7 @@ const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 // Default state
 export const Default = Template.bind({});
 Default.args = {
-  label: "This is the default button! and im changing this for  husky",
+  label: "This is the default button! and im changing ",
 };
 
 // Disabled button state
